5e6d45c19be7bcc23595475b0888b32c99f33329
johnpaoloyao <john.yao@kumu.ph>
2022-06-06 00:01:58 +0800

    feat(env): add env file, edit api to env link, add dotenv package

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import cashout from "./routes/cashout.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = 5000;
 app.use(bodyParser.json());
 app.use(cors());
+dotenv.config({path: './.env'});
 
 app.use("/cashout", cashout);
 
