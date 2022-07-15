7d2d3736966713713b11deb9d0897f4d94305d5b
johnpaoloyao <john.yao@kumu.ph>
2022-07-15 15:23:21 +0800

    feat: update vesta api body

--- routes/vesta.js
+++ routes/vesta.js
@@ -3,12 +3,12 @@ import axios from "axios";
 
 const vesta = express.Router();
 
-vesta.post("/getSessionTags", (_req, res) => {
+vesta.post("/getSessionTags", (req, res) => {
     axios.post(process.env.VESTA_URL+'/GetSessionTags',
         {
             "AccountName" : process.env.VESTA_API_ACCOUNTNAME,
             "Password" : process.env.VESTA_API_PASSWORD,
-            "TransactionID" : "SCQD-CGZT-BRVS-6KE2-VANB-Q9ZB-D5GJ"
+            "TransactionID" : req.body.transactionID
         },
         { headers: 
             {
@@ -25,6 +25,57 @@ vesta.post("/getSessionTags", (_req, res) => {
             console.log(error.status);
             res.send(error);
         })
-})
+});
+
+vesta.post("/chargePaymentRequest", (req, res) => {
+    axios.post(process.env.VESTA_URL+'/ChargePaymentRequest',
+        {
+            "AccountHolderAddressLine1" : "9666 Ignacio Prairie",
+            "AccountHolderCity" : "Gerardobury",
+            "AccountHolderCountryCode" : "US",
+            "AccountHolderFirstName" : "John",
+            "AccountHolderLastName" : "Doe",
+            "AccountHolderPostalCode" : "55742",
+            "AccountHolderRegion" : "Vermont",
+            "RiskInformation" : req.body.riskInformationXml,
+            "AccountHolderAddressLine2" : "Suite 628",
+            "AccountName" : process.env.VESTA_API_ACCOUNTNAME,
+            "AccountNumber" : "340001234527890",
+            "AccountNumberIndicator" : "1",
+            "Amount" : "34.85",
+            "AutoDisposition" : "0",
+            "CVV" : "105",
+            "CipherText" : "F22846F60482457F4F1...",
+            "CreatedByUser" : "John-Doe",
+            "ExpirationMMYY" : "1230",
+            "MerchantRoutingID" : "10",
+            "NumberOfPayments" : "2",
+            "PartnerCustomerKey" : "0GK2-CIQ5-ZA85-ZB14-2URR-GB1V-SIMJ",
+            "Password" : process.env.VESTA_API_PASSWORD,
+            "PaymentDescriptor" : "ChargePaymentRequest",
+            "PaymentSource" : "WEB",
+            "PlanType" : "1",
+            "RecurringArrangementID" : "123XNQCKULS",
+            "ReportingInformation" : "<?xml version='1.0' encoding='UTF-8'?><PartnerReportingAttributes xmlns='http://www.vesta.io/ReportingAttributes' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.vesta.io/ReportingAttributes ReportingAttributes.xsd'><Parameter01>ReportingInformation</Parameter01></PartnerReportingAttributes>",
+            "StoreCard" : "0",
+            "TransactionID" : req.body.transactionID,
+            "WebSessionID" : req.body.webSessionID
+        },
+        { headers: 
+            {
+                'Content-Type': 'application/json',
+                'Accept': '*/*',
+                "Access-Control-Allow-Headers": "content-type",
+                "Access-Control-Allow-Origin": "*",
+            } 
+        }).then(response => {
+            console.log(response.data)
+            res.status(200).send(response.data);
+        }).catch(err => {
+            console.log(err.code);
+            console.log(err.status);
+            res.send(err);
+        })
+});
 
 export default vesta;
